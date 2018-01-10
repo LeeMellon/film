61d6f81be6858bacf8d9c050f1ae55f591f40007
Ian Goodrich and John Rykken <john@>
2018-01-10 15:14:46 -0800

    add innerHTML function to forms

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,19 @@ $(function(){
     console.log(type);
   });
 
-// $(".btn").click(function(){
-//
-//
-// });
 
 
+  $("#form-bt").click(function(event){
+    var filmname = $("input#filmname").val();
+    var director = $("input#director").val();
+    var actor1 = $("input#actor1").val();
+    var actor2 = $("input#actor2").val();
+    var firstname = $("input#firstname").val();
+    // var list = $document.getElementbyId("#film-into-list");
 
+    // $("#film-into-list").appendChild(filmname, director, actor1, actor2, firstname);
+    $("#target").html("<li>" + filmname + "</li>" + "<li>" + director + "</li>" + "<li>" + actor1 + "</li>" + "<li>" + actor2 + "</li>" + "<li>" + firstname + "</li>");
+    console.log(filmname, director, actor1, actor2, firstname);
+    event.preventDefault();
+    });
 });
